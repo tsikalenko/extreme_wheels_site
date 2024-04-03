86b0d730d9beabc9486b9fa010aadfb87e8af69d
Atlas <nikita.tsikalenko@gmail.com>
2024-04-03 18:17:21 +0300

    update events creator

--- client/src/utils/helpers/createEvents.js
+++ client/src/utils/helpers/createEvents.js
@@ -43,12 +43,44 @@ export const createEvents = (data) => {
             enable: true,
         };
         const newEvent2 = {
-            title: '13:30 Персональне ендуро тренування',
+            title: '13:15 Персональне ендуро тренування',
             price: 3000,
             startDate: styledDate,
-            startTime: '13:30',
+            startTime: '13:15',
             endDate: styledDate,
-            endTime: '16:30',
+            endTime: '16:15',
+            description:
+                'Персональне ендуро тренування з тренером на нашому мотоциклі з повним комплектом спорядження',
+            img: 'https://res.cloudinary.com/dd3fd4eey/image/upload/v1701766811/main_gallery/main_gallery_6.jpg',
+            letterSubject: 'Персональне эндуро тренування',
+            letterHtml:
+                'Дякуємо що забронювали тренування!<br/>Найближчим часом ми з Вами зв`яжемось!',
+            fields: [
+                {
+                    name: 'Імя',
+                    description: '',
+                },
+                {
+                    name: 'Номер телефону (Телеграм/Вайбер)',
+                    description: '',
+                },
+                {
+                    name: 'Комментар',
+                    description: '',
+                    isRequired: false,
+                },
+            ],
+            maxQuantity: 1,
+            freeSlots: 1,
+            enable: true,
+        };
+        const newEvent3 = {
+            title: '16:30 Персональне ендуро тренування',
+            price: 3000,
+            startDate: styledDate,
+            startTime: '16:30',
+            endDate: styledDate,
+            endTime: '19:30',
             description:
                 'Персональне ендуро тренування з тренером на нашому мотоциклі з повним комплектом спорядження',
             img: 'https://res.cloudinary.com/dd3fd4eey/image/upload/v1701766811/main_gallery/main_gallery_6.jpg',
@@ -76,6 +108,7 @@ export const createEvents = (data) => {
         };
         createEvent(newEvent1);
         createEvent(newEvent2);
+        createEvent(newEvent3);
         date.setDate(date.getDate() + 1);
     }
 };
